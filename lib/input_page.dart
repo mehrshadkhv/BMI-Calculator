0981bc74b14fbfc9f98e7398f690eb8cefba853b
mehrshadkhv <101244749+mehrshadkhv@users.noreply.github.com>
2025-05-07 09:57:37 +0330

    Third Commit

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -16,34 +21,81 @@ class _InputPageState extends State<InputPage> {
         title: const Text('BMI CALCULATOR'),
         centerTitle: true,
       ),
-      body: const Column(
+      body: Column(
         children: [
-          Expanded(child:
+          const Expanded(child:
           Row(
             children: [
               Expanded(child:
-              ReusableCard(colour: Color(0xFF1D1E33),),
+              ReusableCard(colour: activeCardColour,cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.mars,
+                  size: 80.0,
+                  ),
+                  SizedBox(height: 15.0),
+                  Text('MALE',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Color(0xFF8D8E98),
+                  ),
+                  ),
+                ],
+              ),),
               ),
               Expanded(child:
-              ReusableCard(colour: Color(0xFF1D1E33),),
+              ReusableCard(colour: activeCardColour,cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.venus),
+                  Text('FEMALE'),
+                ],
+              ),),
               ),
             ],
           ),
           ),
-          Expanded(child:
-          ReusableCard(colour: Color(0xFF1D1E33),),
+          const Expanded(child:
+          ReusableCard(colour: activeCardColour,cardChild: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(FontAwesomeIcons.venus),
+              Text('FEMALE'),
+            ],
+          ),),
           ),
-      Expanded(child:
+      const Expanded(child:
           Row(
             children: [
               Expanded(child:
-              ReusableCard(colour: Color(0xFF1D1E33),),
+              ReusableCard(colour: activeCardColour,cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.mars),
+                  Text('MALE'),
+                ],
+              ),),
               ),
               Expanded(child:
-              ReusableCard(colour: Color(0xFF1D1E33),),
+              ReusableCard(colour: activeCardColour,cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.venus),
+                  Text('FEMALE'),
+                ],
+              ),),
           ),
             ],
           ),),
+          Container(
+            color: bottomContainerColour,
+            margin: const EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+            child: const Center(
+              child: Text('CALCULATE'),
+            ),
+          ),
         ],
       ),
     );
@@ -52,8 +104,9 @@ class _InputPageState extends State<InputPage> {
 
 class ReusableCard extends StatelessWidget {
 
-  const ReusableCard({super.key, required this.colour});
+  const ReusableCard({super.key, required this.colour, required this.cardChild});
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -63,6 +116,7 @@ class ReusableCard extends StatelessWidget {
               color: colour,
               borderRadius: BorderRadius.circular(10.0),
             ),
+            child: cardChild,
           );
   }
 }
