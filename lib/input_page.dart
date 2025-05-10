299b9180dec8c6122127b7aef5f2f835431bb1db
mehrshadkhv <101244749+mehrshadkhv@users.noreply.github.com>
2025-05-10 09:44:08 +0330

    Fourth commit

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -10,7 +12,7 @@ class InputPage extends StatefulWidget {
 
   @override
   // ignore: library_private_types_in_public_api
-  _InputPageState createState() => _InputPageState(); 
+  _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
@@ -23,73 +25,90 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Column(
         children: [
-          const Expanded(child:
-          Row(
-            children: [
-              Expanded(child:
-              ReusableCard(colour: activeCardColour,cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(FontAwesomeIcons.mars,
-                  size: 80.0,
+          const Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
                   ),
-                  SizedBox(height: 15.0),
-                  Text('MALE',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Color(0xFF8D8E98),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
                   ),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: ReusableCard(
+              colour: activeCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'HEIGHT',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Color(0xFF8D8E98),
+                    ),
                   ),
                 ],
-              ),),
               ),
-              Expanded(child:
-              ReusableCard(colour: activeCardColour,cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(FontAwesomeIcons.venus),
-                  Text('FEMALE'),
-                ],
-              ),),
-              ),
-            ],
+            ),
           ),
+          const Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'WEIGHT',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'AGE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          const Expanded(child:
-          ReusableCard(colour: activeCardColour,cardChild: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(FontAwesomeIcons.venus),
-              Text('FEMALE'),
-            ],
-          ),),
-          ),
-      const Expanded(child:
-          Row(
-            children: [
-              Expanded(child:
-              ReusableCard(colour: activeCardColour,cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(FontAwesomeIcons.mars),
-                  Text('MALE'),
-                ],
-              ),),
-              ),
-              Expanded(child:
-              ReusableCard(colour: activeCardColour,cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(FontAwesomeIcons.venus),
-                  Text('FEMALE'),
-                ],
-              ),),
-          ),
-            ],
-          ),),
           Container(
             color: bottomContainerColour,
-            margin: const EdgeInsets.only(top: 10.0),
+            margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: bottomContainerHeight,
             child: const Center(
@@ -99,24 +118,5 @@ class _InputPageState extends State<InputPage> {
         ],
       ),
     );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-
-  const ReusableCard({super.key, required this.colour, required this.cardChild});
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-            margin: const EdgeInsets.all(15.0),
-            decoration:  BoxDecoration(
-              color: colour,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: cardChild,
-          );
   }
 }
